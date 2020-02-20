87c05bc4e380420aba086a364d15c53901d5b139
Aldmar <aldmarj@users.noreply.github.com>
2020-02-20 01:21:15 -0600

    Changed port

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ require('dotenv').config({ path: 'variables.env' });
       processMessage(message);
     });
 
-    app.set('port', process.env.PORT || 5000);
+    app.set('port', process.env.PORT || 3000);
     const server = app.listen(app.get('port'), () => {
       console.log(`Express running → PORT ${server.address().port}`);
     })
