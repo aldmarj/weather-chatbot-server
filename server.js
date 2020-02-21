905bffd079db79839675440350010adc6d9c8272
Aldmar <aldmarj@users.noreply.github.com>
2020-02-21 01:24:05 -0600

    expanded whitelist

--- server.js
+++ server.js
@@ -7,9 +7,26 @@ require('dotenv').config({ path: 'variables.env' });
 
     const app = express();
 
-  
+    var allowedOrigins = ['http://aldmar.com',
+                      'http://localhost:8000',
+                      'http://localhost:3000'];
 
     app.use(cors({
+
+      origin: function(origin, callback){
+        // allow requests with no origin
+        // (like mobile apps or curl requests)
+        if(!origin) return callback(null, true);
+        if(allowedOrigins.indexOf(origin) === -1){
+          var msg = 'The CORS policy for this site does not ' +
+                    'allow access from the specified Origin.';
+          return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+      },
+
+      exposedHeaders: ['Content-Length', 'X-Foo', 'X-Bar'],
+
       credentials: true,
     }));
     app.use(bodyParser.json());
