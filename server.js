13f379fa2598466018984221a4d33e8066ed4c77
Aldmar <aldmarj@users.noreply.github.com>
2020-03-05 22:36:00 -0600

    Allowing credentials

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ require('dotenv').config({ path: 'variables.env' });
 
     app.use(function(req, res, next) {
       res.header("Access-Control-Allow-Origin", "https://www.aldmar.com");
+      res.header("Access-Control-Allow-Credentials", 'true');
       res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
       next();
