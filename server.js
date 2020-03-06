8601c4fc20decde62b3c4df7bad777cc70480b52
Aldmar <aldmarj@users.noreply.github.com>
2020-03-05 23:21:07 -0600

    Changed order of app.use

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ require('dotenv').config({ path: 'variables.env' });
           res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
           
           if (req.method === 'OPTIONS'){
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
             res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
             return res.status(200).json({});
           }
