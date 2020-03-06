f33d3e6fc243d4ff7d4252ef8d0da5bdcfd948ee
Aldmar <aldmarj@users.noreply.github.com>
2020-03-05 22:23:11 -0600

    Still working on CORS

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ require('dotenv').config({ path: 'variables.env' });
     app.use(bodyParser.urlencoded({ extended: true }));
 
     app.use(function(req, res, next) {
-      res.header("Access-Control-Allow-Origin", "https://aldmar.com");
+      res.header("Access-Control-Allow-Origin", "*");
       res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
       next();
