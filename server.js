24c29dd3c02e36f0535fac8ee90c97104e84dc59
Aldmar <aldmarj@users.noreply.github.com>
2020-03-05 23:02:56 -0600

    Checking for OPTIONS

--- server.js
+++ server.js
@@ -13,8 +13,12 @@ require('dotenv').config({ path: 'variables.env' });
     app.use(function(req, res, next) {
       res.header("Access-Control-Allow-Origin", "https://www.aldmar.com");
       res.header("Access-Control-Allow-Credentials", 'true');
-      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      
+      if (req.method === "OPTIONS"){
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+        return res.status(200).json({});
+      }
       next();
     });
     
