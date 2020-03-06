502867c4a22c50caccd31343efd2c71ededda89c
Aldmar <aldmarj@users.noreply.github.com>
2020-03-05 23:15:33 -0600

    Trying all origins

--- server.js
+++ server.js
@@ -11,14 +11,14 @@ require('dotenv').config({ path: 'variables.env' });
     app.use(bodyParser.urlencoded({ extended: true }));
 
     app.use(function(req, res, next) {
-      res.header("Access-Control-Allow-Origin", "https://www.aldmar.com");
+      res.header("Access-Control-Allow-Origin", "*");
       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
       
       if (req.method === 'OPTIONS'){
         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
         return res.status(200).json({});
       }
-      
+
       next();
     });
     
